b829a60824570d22f0cce11425d3ff3500e380f5
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-06-25 21:00:33 +0200

    workflow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use log::info;
+use std::{env, str::FromStr};
 use teloxide::{update_listeners::webhooks, Bot};
+use url::Url;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     let bot: Bot = Bot::from_env();
     let addr = ([127, 0, 0, 1], 8000).into();
-    let url = "https://2053-90-94-189-135.eu.ngrok.io".parse().unwrap();
+    let url = env::var("url").expect("URL is not set");
+    let url = Url::from_str(&url).unwrap();
+    // let url = "https://84c1-90-94-189-135.eu.ngrok.io".parse().unwrap();
     // ngrok http 8000
     let listener = webhooks::axum(bot.clone(), webhooks::Options::new(addr, url))
         .await
